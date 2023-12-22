003638e901220d7337922c0dfc91c301f1c0d675
GitPoomCmu <152594220+GitPoomCmu@users.noreply.github.com>
2023-12-22 16:22:30 +0700

    Update lab11_1.cpp

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,16 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+using namespace std;
+
+void fortune(){
+    string grade[9] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+    srand(time(0));
+    int date = rand()%997978 + 2023;
+    cout << "You will get " << grade[(rand()%9)] << " in this " << date << ".";
+}
+
+int main(){
+    cout << "Press Enter 3 times to reveal your future.\n";
+    for(int i=0; i<3; i++) cin.get();
+    fortune();
+    return 0;
+}
